d2178b9c66bb6ae744b4f92f9dd954a35b2fae75
Jason <jasonkrentsel@rocketmail.com>
2018-05-21 10:58:03 -0700

    assets

---
+++ core/assets/Levels/dirt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dirt" tilewidth="96" tileheight="96" tilecount="1" columns="1">
+ <image source="b_dirt_resized.png" width="96" height="96"/>
+</tileset>
